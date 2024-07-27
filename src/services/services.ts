35ae686ca124ed61648e0c8224af850609ee5638
majid kia <majidkia.dev@gmail.com>
2024-07-27 14:07:00 +0330

    feat: :alien: add services for api calls

---
+++ src/services/services.ts
@@ -0,0 +1,8 @@
+const baseUrl = "https://fakestoreapi.com";
+export const getProductsData = async () => {
+  const res = await fetch(`${baseUrl}/products`);
+  if (!res.ok) {
+    throw new Error("Failed to fetch user data");
+  }
+  return res.json();
+};
